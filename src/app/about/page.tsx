34b1a77e4a0fcd19055b3513566a743819d09ed1
Hüsam <husamahmud@gmail.com>
2025-05-29 21:39:23 +0300

    add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,33 @@
 const Page = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="container mx-auto flex flex-1 flex-col items-center justify-center gap-10 text-center">
+      <div className="font-dm-sans flex max-w-7xl flex-1 flex-col justify-center text-3xl font-extralight">
+        <p>
+          Set against a tranquil light blue backdrop, cascading drips and delicate streaks of green, red, yellow,
+          orange, and pink create a rhythmic vertical flow—evoking the graceful descent of rain or even the pulse of a
+          heartbeat.
+        </p>
+        <br />
+        <p>
+          The painting gives a sense of movement and fluidity, with the vertical lines resembling paint dripping down
+          the canvas.
+        </p>
+        <br />
+        <p>A perfect statement for collectors seeking art that resonates with emotional elegant complexity.</p>
+        <br />
+        <p className="pt-20">
+          Set against a tranquil light blue backdrop, cascading drips and delicate streaks of green, red, yellow,
+          orange, and pink create a rhythmic vertical flow—evoking the graceful descent of rain or even the pulse of a
+          heartbeat.
+        </p>
+      </div>
+
+      <a
+        href="mailto:contact@antje-k.art"
+        className="pb-5 font-light text-[#828282] underline"
+      >
+        contact@antje-k.art
+      </a>
     </div>
   )
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,16 @@
 const Page = () => {
-  return <div>Contact</div>
+  return (
+    <div className="container mx-auto flex flex-1 flex-col items-center justify-center gap-5">
+      <p className="text-[26px] text-[#151515]">Reach Out:</p>
+
+      <a
+        href="mailto:contact@antje-k.art"
+        className="text-[26px] font-light text-[#151515] underline"
+      >
+        contact@antje-k.art
+      </a>
+    </div>
+  )
 }
 
 export default Page
