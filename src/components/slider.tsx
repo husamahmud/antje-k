0582536efc1f202fede1ec41d3932e7d0b543aa6
Hüsam <husamahmud@gmail.com>
2025-06-21 18:45:39 +0300

    update blur

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -137,7 +137,7 @@ const SlideContent = memo(
               className="absolute top-1/2 left-1/2 aspect-square max-h-[350px] w-fit max-w-full -translate-x-1/2 -translate-y-1/2 scale-95 object-contain transition-all ease-out md:max-h-[550] md:scale-100"
               style={{
                 opacity: isActive ? 0.8 : 0,
-                filter: isActive ? (isMobile ? 'blur(3px) saturate(1)' : 'blur(15px) saturate(1.6)') : 'none',
+                filter: isActive ? (isMobile ? 'blur(10px) saturate(1)' : 'blur(15px) saturate(1.6)') : 'none',
                 zIndex: 0,
                 transitionDuration: isMobile ? '200ms' : '300ms',
                 willChange: 'opacity, filter',
